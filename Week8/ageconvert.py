51f9538ea254765d529058244af3d43939c511a4
bansuri0100 <bansuri0100@gmail.com>
2019-11-12 18:35:55 +0530

    Made simpler

--- Week8/ageconvert.py
+++ Week8/ageconvert.py
@@ -1,6 +1,9 @@
+import datetime
+x=datetime.datetime.now()
 def AgeConvert(dob):
     dob = dob.split("-")
-    age = 2019 - int(dob[2])
+
+    age = x.year - int(dob[2])
     if int(dob[1])>6:
         age = age - 1
     print("Age :",age)
