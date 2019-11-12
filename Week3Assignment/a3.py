ac19b519d07f900d99cd8825e76a91d39132c934
bansuri0100 <bansuri0100@gmail.com>
2019-11-12 19:01:13 +0530

    Rename 4.py

--- Week3Assignment/a3.py
+++ Week3Assignment/a3.py
@@ -1,6 +1,6 @@
 dict={1:"Hydrogen",2:"Helium",3:"Lithium",5:"Boron"}
-def AtomicDictionary():							#Run another file 4.py and it will give same results.
-	while True:							#AtomicDictionary() is being called from 4.py file as well.	
+def AtomicDictionary():							#Run another file Atomic.py and it will give same results.
+	while True:							#AtomicDictionary() is being called from Atomic.py file as well.	
 		print("Enter the atomic number.Enter 0 to stop :")	#At the time of run time, try giving duplicate and unique values.
 		num=int(input())					#Duplicate keys wont be added, while duplicate values will be added
 		if(num==0):						#Unique values will be added.
